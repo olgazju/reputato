f48bf46b332c21dae58f5c7fd06cf851bbd0c178
Olga Braginskaya <olgazjuzju@gmail.com>
2025-05-12 15:32:46 +0300

    removed nodejs

--- backend/build.sh
+++ backend/build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-curl -fsSL https://deb.nodesource.com/setup_23.x | bash -
-apt-get install -y nodejs
 npm install -g @brightdata/mcp
 pip install --upgrade pip
 pip install -r requirements.txt
