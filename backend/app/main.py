cb7e4233d2a7f8fcac64074f91c2ea6237ee825c
Olga Braginskaya <olgazjuzju@gmail.com>
2025-05-23 19:29:58 +0300

    added origin url as env variable

--- backend/app/main.py
+++ backend/app/main.py
@@ -181,7 +181,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://reputato-jdrzv9otwhstyktkmwqpyd.streamlit.app"],
+    allow_origins=[os.getenv("FRONTEND_URL")],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
